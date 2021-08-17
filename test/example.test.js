74ef1feab82c002376c7108caee5944881890a42
yosefkirkcohen <ycohen45@yahoo.com>
2021-08-17 15:27:40 -0700

    working tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,8 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { comparesNumbers } from "../utils.js";
+
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
@@ -24,7 +26,35 @@ test('test for larger than', (expect) => {
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = comparesNumbers(18,10);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('test for less than', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = -1;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = comparesNumbers(4,10);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('test for equal', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 0;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = comparesNumbers(10,10);
 
     //Expect
     // Make assertions about what is expected versus the actual result
